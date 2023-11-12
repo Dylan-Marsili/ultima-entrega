522cfa155ee9af4e15a799bb5926c2a5f03cff4d
= <=>
2023-11-12 18:26:23 -0300

    register

--- AppBlog/forms.py
+++ AppBlog/forms.py
@@ -9,10 +9,13 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(label='Email')
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+    description = forms.CharField(label="Descripción", max_length=200, required=False)
+    link = forms.CharField(
+        label="Link", max_length=200, required=False)
  
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'description', 'link']
 
 
 class Profile(UserChangeForm):
